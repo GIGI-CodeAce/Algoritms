c2b2708e1f9654b449f9f3a0b876c2a29ffced82
Dobre Robert <dobrerobert441@gmail.com>
2024-12-23 10:14:01 +0200

    vowels added

---
+++ vowels.js
@@ -0,0 +1,18 @@
+
+function findVowels(str){
+    let strObj = {}
+    let vowels = 0
+    str = str.toLowerCase()
+    for(let char of str){
+        strObj[char] = (strObj[char] || 0) + 1
+    }
+    for(let key in strObj){
+        'aeiou'.includes(key) ? vowels += strObj[key] : vowels
+        console.log(key,strObj[key]);
+        
+    }
+
+    return vowels
+}
+
+console.log(findVowels("Mountain Climbers"));
