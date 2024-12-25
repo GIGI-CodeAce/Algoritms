2c929a39a04e0a05288a576f86878926f322fe63
Dobre Robert <dobrerobert441@gmail.com>
2024-12-25 10:04:47 +0200

    stringPattern

---
+++ stringPattern.js
@@ -0,0 +1,17 @@
+
+function steps(n,char){
+    for(let row=0;row<n;row++){
+        let pattern = ""
+        for(let col=0;col<n;col++){
+            if(row<=col){
+                pattern += char
+            }else{
+                pattern += ' '
+            }
+        }
+        console.log(pattern);
+        
+    }
+}
+
+steps(30, '=')
